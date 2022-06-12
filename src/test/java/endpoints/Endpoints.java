2f1502b69dec1bac94d3aa58a9901db645076a27
screen190795 <horizonjournal78@gmail.com>
2022-06-12 04:10:02 +0300

    Обновление эндпоинтов

--- src/test/java/endpoints/Endpoints.java
+++ src/test/java/endpoints/Endpoints.java
@@ -1,6 +1,8 @@
 package endpoints;
 
 public class Endpoints {
-    public static final String bookings = "/bookings/";
+    public static final String booking = "/booking";
     public static final String createToken = "/auth";
+    public static final String healthCheck = "/ping";
+
 }
