3c28c145b293c8f9aaa77bbb482fd41bf21c23b7
screen190795 <horizonjournal78@gmail.com>
2022-06-12 04:07:04 +0300

    Обновление аннотации Sources

--- src/test/java/config/ServerConfig.java
+++ src/test/java/config/ServerConfig.java
@@ -2,8 +2,9 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-import static org.aeonbits.owner.Config.*;
-@Sources({"${propertiesFile}", "classpath:application.properties"})
+import static org.aeonbits.owner.Config.Sources;
+
+@Sources("file:src/test/java/config/application.properties")
 public interface ServerConfig extends Config {
     @Key("restful.booker.username")
     String restful_booker_username();
