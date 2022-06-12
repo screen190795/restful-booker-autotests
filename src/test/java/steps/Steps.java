2111816e5343930e2024a7cd7d27c45c70a199dd
screen190795 <horizonjournal78@gmail.com>
2022-06-12 04:13:34 +0300

    Добавление шагов

--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -1,6 +1,5 @@
 package steps;
 
-import config.ServerConfig;
 import endpoints.Endpoints;
 import io.qameta.allure.Step;
 import objects.User;
@@ -10,11 +9,9 @@ import static io.restassured.RestAssured.given;
 
 
 public class Steps {
-    private ServerConfig config;
-
     @Step("переход на сайт, авторизация")
-    public String goBookerCreateToken() {
-        User user = new User(config.restful_booker_username(), config.restful_booker_password());
+    public String goBookerCreateToken(String username, String password) {
+        User user = new User(username, password);
         return given()
                 .spec(Specifications.requestSpec())
                 .body(user)
@@ -22,6 +19,20 @@ public class Steps {
                 .post(Endpoints.createToken)
                 .then()
                 .log().body()
-                .extract().body().as(String.class);
+                .extract().body().jsonPath().getString("token");
+    }
+
+    @Step("переход на сайт, авторизация")
+    public String goBookerCreateInvalidToken(String username, String password) {
+        User user = new User(username, password);
+        return given()
+                .spec(Specifications.requestSpec())
+                .body(user)
+                .when()
+                .post(Endpoints.createToken)
+                .then()
+                .spec(Specifications.responseSpec())
+                .log().status().log().body()
+                .extract().body().jsonPath().getString("reason");
     }
 }
