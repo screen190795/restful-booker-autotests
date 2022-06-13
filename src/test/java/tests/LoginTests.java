cf07a5ad5ca923dcf5fe876a7562018bac486d01
screen190795 <horizonjournal78@gmail.com>
2022-06-13 03:22:48 +0300

    Переименование класса

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -2,27 +2,27 @@ package tests;
 
 import config.ServerConfig;
 import org.aeonbits.owner.ConfigFactory;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import steps.Steps;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
-public class ApiLoginTests {
+public class LoginTests {
     ServerConfig config = ConfigFactory.create(ServerConfig.class);
     Steps steps = new Steps();
 
     @Test
     public void userLoginSuccessful() {
         String token = steps.goBookerCreateToken(config.restful_booker_username(), config.restful_booker_password());
-        Assert.assertNotNull(token);
+        assertNotNull(token);
     }
 
     @Test
     public void userLoginInvalidUsername() {
         String token = steps.goBookerCreateInvalidToken(randomAlphanumeric(10), config.restful_booker_password());
-        Assert.assertNotNull(token);
+        assertNotNull(token);
     }
 
     @Test
