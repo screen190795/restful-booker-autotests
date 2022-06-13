47cc6d106f318d6b73c50133c8c42c2ab7dc0c59
screen190795 <horizonjournal78@gmail.com>
2022-06-13 23:06:52 +0300

    Добавление новых шагов

--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -1,11 +1,17 @@
 package steps;
 
-import endpoints.Endpoints;
 import io.qameta.allure.Step;
+import objects.Booking;
 import objects.User;
-import specifications.Specifications;
 
+import java.util.List;
+import java.util.Map;
+
+import static endpoints.Endpoints.booking;
+import static endpoints.Endpoints.createToken;
 import static io.restassured.RestAssured.given;
+import static specifications.Specifications.requestSpec;
+import static specifications.Specifications.responseSpec;
 
 
 public class Steps {
@@ -13,10 +19,10 @@ public class Steps {
     public String goBookerCreateToken(String username, String password) {
         User user = new User(username, password);
         return given()
-                .spec(Specifications.requestSpec())
+                .spec(requestSpec())
                 .body(user)
                 .when()
-                .post(Endpoints.createToken)
+                .post(createToken)
                 .then()
                 .log().body()
                 .extract().body().jsonPath().getString("token");
@@ -26,13 +32,53 @@ public class Steps {
     public String goBookerCreateInvalidToken(String username, String password) {
         User user = new User(username, password);
         return given()
-                .spec(Specifications.requestSpec())
+                .spec(requestSpec())
                 .body(user)
                 .when()
-                .post(Endpoints.createToken)
+                .post(createToken)
                 .then()
-                .spec(Specifications.responseSpec())
+                .spec(responseSpec())
                 .log().status().log().body()
                 .extract().body().jsonPath().getString("reason");
     }
+
+    @Step("получение списка всех id бронирований")
+    public List<Integer> getBookingIds() {
+        return given()
+                .spec(requestSpec())
+                .when()
+                .get(booking)
+                .then()
+                .spec(responseSpec())
+                .log().status()
+                .extract().body().jsonPath().getList("bookingid");
+    }
+
+    @Step("получение бронирования по id")
+    public Booking getBooking(String bookingId) {
+        return given()
+                .spec(requestSpec())
+                .when()
+                .get(booking + "/" + bookingId)
+                .then()
+                .spec(responseSpec())
+                .log().status().log().body()
+                .extract().body().as(Booking.class);
+    }
+
+    @Step("получение списка всех id бронирований с параметрами фильтра")
+    public List<Integer> getBookingIdsByParams(Map<String, Object> paramsMap) {
+        return given()
+                .spec(requestSpec())
+                .params(paramsMap)
+                .when()
+                .get(booking)
+                .then()
+                .spec(responseSpec())
+                .log().status()
+                .log().body()
+                .extract().body()
+                .jsonPath().getList("bookingid");
+    }
+
 }
